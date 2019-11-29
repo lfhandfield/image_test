dd647ce5c7f741a6d647bc85eb1e9eff781402c2
lfhandfield <lfhandfield@gmail.com>
2019-11-29 13:44:15 +0000

    Update main.h

--- src/main.h
+++ src/main.h
@@ -12,10 +12,27 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_net.h>
 #include "bastructs.h"
+#include "display.h"
 
 using namespace LFHPrimitive;
+
+class DefaultRessourceLoader : public LFHDisplay::RessourceLoader{
+    public:
+    GLuint textures[10];
+
+    DefaultRessourceLoader();
+    void loadTexture(char* path, GLuint& where, int flag);
+    void useTexture(const GUITEXTURES_enum);
+    void useSound(const GUISOUND_enum);
+    void useTextureExt(unsigned int ID);
+    void allocTextureExt(unsigned int ID);
+    void deallocTextureExt(unsigned int ID);
+};
+
+
 class Task{
 	public:
+	LFHDisplay::MyWindow* dawin;
 	Tuple<uint32_t, 5> data;
 };
 
