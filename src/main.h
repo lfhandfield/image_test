a0c247672836ba8806a102793dbafe0c02a11dcb
lfhandfield <lfhandfield@gmail.com>
2019-12-02 11:12:53 +0000

    Update main.h

--- src/main.h
+++ src/main.h
@@ -30,9 +30,16 @@ class DefaultRessourceLoader : public LFHDisplay::RessourceLoader{
 };
 
 
-class Task{
+class Task : public LFHPrimitive::ArgumentParser, public LFHDisplay::ProcessState, public LFHDisplay::renderMode{
 	public:
 	LFHDisplay::MyWindow* dawin;
 	Tuple<uint32_t, 5> data;
+	TASK_MEMBER_DEFINITIONS
+        int OnKeyDown(const SDL_KeyboardEvent& Event); //OnKeyDown(Event.key.keysym.sym,Event.key.keysym.mod,Event.key.keysym.unicode);
+        int OnKeyUp(const SDL_KeyboardEvent& Event); //OnKeyUp(Event.key.keysym.sym,Event.key.keysym.mod,Event.key.keysym.unicode);
+        int OnMaintain();//OnKeyUp(Event.key.keysym.sym,Event.key.keysym.mod,Event.key.keysym.unicode);
+        int listen(LFHDisplay::GUImessage&);
+	void draw(MyWindow*);
+	void drawAlias(MyWindow*);
 };
 
