880c57ca93a161146ad3800ff5f41d4cef5c2321
lfhandfield <lfhandfield@gmail.com>
2019-12-02 12:38:14 +0000

    Update main.h

--- src/main.h
+++ src/main.h
@@ -1,17 +1,3 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cstdint>
-#include <cinttypes>
-#include <thread>
-#include <mutex>
-
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GL/glext.h>
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_net.h>
-#include "bastructs.h"
 #include "Display.h"
 
 using namespace LFHPrimitive;
