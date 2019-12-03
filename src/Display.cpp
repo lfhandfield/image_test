d2d33be12c1ddc46e0a0e8d4f0d33e005555c97d
lfhandfield <lfhandfield@gmail.com>
2019-12-03 12:30:50 +0000

    Update Display.cpp

--- src/Display.cpp
+++ src/Display.cpp
@@ -2371,6 +2371,8 @@ void GUIStyle::setToMenuDefault(){
         exit(-1);
     }*/
 
+	LFH_ALIVE;
+	if (glClearColor == NULL) printf("function is still not linked...!!!\n");
   //  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   //  SDL_RenderClear(renderer);
   //  SDL_RenderPresent(renderer);
