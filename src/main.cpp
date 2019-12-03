4feffc46a13564e7cee68edf2fead0a6fe61db20
lfhandfield <lfhandfield@gmail.com>
2019-12-03 17:00:15 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -70,14 +70,14 @@ int main(int argc, char** argv){
 task(argc, argv);}
 
 Task::Task() : dd_menu((uint32_t)GUIID_MAIN_DROP,(uint32_t)GSTYID_TEXTBOX){
-   	ctrl_state.gui_styles[GSTYID_TEXTBOX].setColorHIS(0,0.0f,0.5f,1.0f);
-	ctrl_state.gui_styles[GSTYID_TEXTBOX].setColorHIS(1,0.0f,0.5f,1.0f);
-	ctrl_state.gui_styles[GSTYID_TEXTBOX].setColorHIS(2,0.8f,0.5f,1.0f);
-	ctrl_state.gui_styles[GSTYID_TEXTBOX].setColorHIS(3,0.0f,0.5f,1.0f);
-	ctrl_state.gui_styles[GSTYID_TEXTBOX].setBorderSizes(16,16);
+   	LFHDisplay::ctrl_state.gui_styles[GSTYID_TEXTBOX].setColorHIS(0,0.0f,0.5f,1.0f);
+	LFHDisplay::ctrl_state.gui_styles[GSTYID_TEXTBOX].setColorHIS(1,0.0f,0.5f,1.0f);
+	LFHDisplay::ctrl_state.gui_styles[GSTYID_TEXTBOX].setColorHIS(2,0.8f,0.5f,1.0f);
+	LFHDisplay::ctrl_state.gui_styles[GSTYID_TEXTBOX].setColorHIS(3,0.0f,0.5f,1.0f);
+	LFHDisplay::ctrl_state.gui_styles[GSTYID_TEXTBOX].setBorderSizes(16,16);
 	    dd_menu.rect[2] =312;
 	    dd_menu.rect[3] =212;
-	    dd_menu.setPositionRelativeTo(1,RELPOS_TL_CORNER,0,0);
+	    dd_menu.setPositionRelativeTo(1,LFHDisplay::RELPOS_TL_CORNER,0,0);
 	dawin->insertGUI(&dd_menu);
 
         Vector<char*> listoptions;
